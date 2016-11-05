918ea24d31e95821678295ce7a435e5d2127de4d
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-11-05 20:02:56 +0100

    Add game-engine

--- src/model.js
+++ src/model.js
@@ -1,10 +1,50 @@
+const gameEngine = require('./game/game-engine')
 const reducers = require('./reducers')
 const effects = require('./effects')
+
+const gameState = gameEngine.init({
+  tiles: [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ],
+  entities: [
+    {
+      id: 'robot',
+      components: {
+        position: { x: 4, y: 5 },
+        sprite: { url: 'assets/robot.png' }
+      }
+    },
+    {
+      components: {
+        position: { x: 2, y: 8 },
+        item: { type: 'gem' },
+        sprite: { url: 'assets/gem.png' }
+      }
+    },
+    {
+      components: {
+        position: { x: 7, y: 2 },
+        item: { type: 'gem' },
+        sprite: { url: 'assets/gem.png' }
+      }
+    }
+  ]
+})
 
 const initialState = {
   robot: { x: 5, y: 5 },
   running: false,
-  srcCode: ''
+  srcCode: '',
+  game: gameState
 }
 
 module.exports = {

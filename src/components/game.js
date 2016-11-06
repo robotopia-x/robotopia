6ce7f5cc407d88611ac22a7f9883ce45ec6199c0
timgrossmann <t.grossmann1994@web.de>
2016-11-06 12:26:16 +0100

    sorting the entities to prevent wrong overlapping

--- src/components/game.js
+++ src/components/game.js
@@ -28,11 +28,7 @@ function render (ctx, width, height, state) {
   ctx.clearRect(0, 0, width, height)
   renderTiles(ctx, state.tiles)
 
-  _.forEach(gameEngine.getAllEntities(['position', 'sprite'], state), (entity) => {
-    const { position, sprite } = entity
-
-    ctx.drawImage(assets.store[sprite.type], position.x * TILE_WIDTH, position.y * TILE_HEIGHT)
-  })
+  renderEntities(ctx, state)
 
   ctx.restore()
 }
@@ -43,6 +39,18 @@ function renderTiles (ctx, tiles) {
       ctx.drawImage(getTileImage(tiles[y][x]), x * TILE_WIDTH, y * TILE_HEIGHT + 40)
     }
   }
+}
+
+function renderEntities (ctx, state) {
+  const sortedEntities = _.sortBy(gameEngine.getAllEntities(['position', 'sprite'], state), [(e) => {
+    return [e.position.x, e.position.y]
+  }])
+
+  _.forEach(sortedEntities, (entity) => {
+    const { position, sprite } = entity
+
+    ctx.drawImage(assets.store[sprite.type], position.x * TILE_WIDTH, position.y * TILE_HEIGHT)
+  })
 }
 
 function getTileImage (type) {

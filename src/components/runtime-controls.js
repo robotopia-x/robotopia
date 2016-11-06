15b1db63833288cde373c9b75010970cf9173bb0
timgrossmann <t.grossmann1994@web.de>
2016-11-06 11:23:26 +0100

    gems resized / small changes on the gui design

--- src/components/runtime-controls.js
+++ src/components/runtime-controls.js
@@ -1,14 +1,35 @@
 const html = require('choo/html')
+const sf = require('sheetify')
+
+const prefix = sf`
+  :host {
+    height: 30px;
+    width: 75px;
+    color: ##404040;
+    background-color: #dddddd;
+    border: none;
+    border-radius: 5%;
+    font-size: 14px;
+    font-family: Helvetica, Arial, Sans-Serif;
+    text-decoration: none;
+    vertical-align: middle;
+  }
+  
+  :host:hover {
+    color: #2b2b2b;
+    background-color: white;
+  }
+`
 
 function runtimeControlsView (state, prev, send) {
   if (state.running) {
     return html`
-      <button disabled>running...</button>
+      <button id="disabled" class=${prefix} disabled>Running...</button>
    `
   }
 
   return html`
-    <button onclick=${() => send('runCode', { })}>run</button>
+    <button id="clickable" class=${prefix} onclick=${() => send('runCode', { })}>► Run</button>
   `
 }
 

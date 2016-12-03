a31e8acfa5f695b0253a3a9eb948773cfe6b6951
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-12-03 12:45:21 +0100

    Set namespace of game fix
    It would be nicer to have this configurable but we would need to
    rewrite the name of send actions triggered by components.
    maybe we can come back to this later

--- src/game/model.js
+++ src/game/model.js
@@ -5,7 +5,6 @@ const { markerCreator } = require('./components/marker-creator')
 
 module.exports = game.engine({
   state: initialState,
-  namespace: 'game',
   components: {
     movable,
     markerCreator

5104f8e9b3bda8e91d48f9405fe428a82ed4786a
timgrossmann <t.grossmann1994@web.de>
2016-12-07 10:56:42 +0100

    gems are now collecatble from a collector (robot) / gems will be deleted

---
+++ src/game/components/collector.js
@@ -0,0 +1,34 @@
+const _ = require('lodash')
+
+module.exports = {
+  collector: {
+    requires: ['position'],
+
+    effects: {
+      collectResource: (data, state, game, send) => {
+        const collectorId = state.id
+        const resource = getEntitiyFromPos({
+          collectorId: collectorId,
+          x: state.position.x,
+          y: state.position.y
+        }, game)
+
+        if (resource) {
+          send('game:deleteEntity', {
+            data: { id: resource.id }
+          }, _.noop)
+        }
+
+        return {}
+      }
+    }
+  }
+}
+
+function getEntitiyFromPos ({ collectorId, x, y }, state) {
+  return _.find(state.entities, (entity) => {
+    return (entity.id !== collectorId &&
+    entity.position.x === x &&
+    entity.position.y === y)
+  })
+}

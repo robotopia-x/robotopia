a7b22518582347b54f001ad3d573b3c00baa4806
timgrossmann <t.grossmann1994@web.de>
2016-12-19 14:30:29 +0100

    pathfinding now uses rotation -> the character alwasy walks forward

--- src/utils/robot-api.js
+++ src/utils/robot-api.js
@@ -9,29 +9,21 @@ module.exports = {
   actions: {
     move: (direction) => ['game:movable.move', { direction: MOVE[direction] }],
     rotate: (direction) => ['game:movable.rotate', { direction: ROTATE[direction] }],
-    setRotation: (direction) => ['game:movable.setRotation', { direction: ROTATE[direction] }],
+    setRotation: (direction) => ['game:movable.setRotation', { direction: direction }],
     placeMarker: () => ['game:spawner.spawn'],
     collectResource: () => ['game:collector.collectResource']
   },
   functions: {
     moveTo: function (xPos, yPos) {
       var entityPos = this.getPosition()
-      var path = pathfinder.getMovementCommands(
-        { x: entityPos.x, y: entityPos.y },
-        { x: xPos, y: yPos },
-        entityPos.rotation)
+      var path = pathfinder.getMovementCommands({ x: entityPos.x, y: entityPos.y }, { x: xPos, y: yPos })
 
       for (var i = 0; i < path.length; i++) {
-        var command = path[i]
-
-        if (command.move) {
-          this.move(command.move)
-        }
-
-        if (command.setRotate) {
-          this.setRotation(command.setRotate)
-        }
+        this.setRotation(path[i])
+        this.move('FORWARD')
       }
+
+      this.setRotation(entityPos.rotation)
     }
   },
   sensors: {

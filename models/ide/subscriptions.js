a8070dbbf72cda394df7355540cd6b13a504b036
Paul Sonnentag <paul.sonnentag@googlemail.com>
2017-01-02 22:33:20 +0100

    Add clock model

--- models/ide/subscriptions.js
+++ models/ide/subscriptions.js
@@ -1,19 +1,7 @@
-const _ = require('lodash')
-const clock = require('../../lib/utils/clock')
 const robotRuntime = require('../../lib/utils/robot-runtime')
 const robotApi = require('../../lib/utils/robot-api')
 
 module.exports = {
-  clock: (send) => {
-    clock.onTick(() => {
-      send('stepRobotRuntime', _.noop)
-    })
-
-    clock.onProgress((progress) => {
-      send('game:setStepProgress', { progress }, _.noop)
-    })
-  },
-
   robotRuntime: (send) => {
     robotRuntime.connect(send)
     robotRuntime.createRobot({

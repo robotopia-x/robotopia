899c7c59deeb7b838f10d658bf2cf9feb1c05abd
timgrossmann <t.grossmann1994@web.de>
2017-01-04 12:48:32 +0100

    toolbox can now be updated for each level of the tutorials

--- models/game/tutorials/index.js
+++ models/game/tutorials/index.js
@@ -4,12 +4,26 @@ const { collector } = require('../components/collector')
 const { spawner } = require('../components/spawner')
 const reducers = require('../reducers')
 
-const initialState = require('../initial-state')
+const initialState = require('./initial-state')
 const tutorial1State = require('./tutorial-1-state')
 const tutorial2State = require('./tutorial-2-state')
 
 module.exports = [
-  initialState,
-  tutorial1State,
-  tutorial2State
+  {
+    state: initialState.state,
+    workspace: initialState.workspace,
+    toolbox: initialState.toolbox
+  },
+
+  {
+    state: tutorial1State.state,
+    workspace: tutorial1State.workspace,
+    toolbox: tutorial1State.toolbox
+  },
+
+  {
+    state: tutorial2State.state,
+    workspace: tutorial2State.workspace,
+    toolbox: tutorial2State.toolbox
+  }
 ]
--- models/game/tutorials/tutorial-1-state.js
+++ models/game/tutorials/tutorial-1-state.js
@@ -1,23 +1,32 @@
 const entities = require('../entities')
 
 module.exports = {
-  tiles: [
-    [1, 0, 1, 1, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1],
-    [1, 1, 1, 0, 1, 1, 1]
-  ],
+  state: {
+    tiles: [
+      [1, 0, 1, 1, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1],
+      [1, 1, 1, 0, 1, 1, 1]
+    ],
 
-  stepProgress: 0,
+    stepProgress: 0,
 
-  entities: [
-    entities.base({ x: 2, y: 5, id: 'BASE' }),
-    entities.robot({ x: 2, y: 5, id: 'ROBOT'}),
-    entities.gem({ x: 0, y: 0 })
-  ]
+    entities: [
+      entities.base({x: 2, y: 5, id: 'BASE'}),
+      entities.robot({x: 2, y: 5, id: 'ROBOT'}),
+      entities.gem({x: 0, y: 0})
+    ]
+  },
+  workspace: '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="move" id="UqpR4v`8/i!re(x3~{AQ" x="155" y="305"><field name="Move">FORWARD</field><next><block type="move" id="x[FVw3mwV4%7/;vvs1XH"><field name="Move">FORWARD</field><next><block type="move" id="`2}Nw53YzN$!$f+vf#Sg"><field name="Move">FORWARD</field></block></next></block></next></block></xml>',
+  toolbox: `<xml id="toolbox" style="display: none">
+                <category name="Logic" colour="100">
+                  <block type="controls_repeat_ext"></block>
+                  <block type="math_number"></block>
+                </category>
+           </xml>`
 }

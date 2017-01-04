79ab169c585e32b04a95d48eb71316cc0eae02a6
timgrossmann <t.grossmann1994@web.de>
2017-01-04 16:22:24 +0100

    story modal at the start of every tutorial level

--- elements/game/winningCondition.js
+++ elements/game/winningCondition.js
@@ -1,5 +1,6 @@
 const html = require('choo/html')
 const sf = require('sheetify')
+const _ = require('lodash')
 
 const winPrefix = sf`
   :host {
@@ -26,14 +27,14 @@ const goalPrefix = sf`
     position: absolute;
     left: 80%;
     top: 0;
-    height: 1%;
+    height: 50px;
     width: 20%;
-    background-color: grey;
+    background-color: #DDDDDD;
     color: transparent;
   }
   
   :host:hover {
-    height: 15%;
+    height: 20%;
     color: #404040;
   }
   
@@ -50,21 +51,43 @@ const winningCondition = (state, send) => {
     return html`
     <div class="${winPrefix}">
       <div class="modalContent">
-        <h1>Your content here!</h1>  
+        <h1>Congratulations on finishing Level ${level.level + 1}</h1>  
         <button onclick=${() => send('nextLevel')}>Next Level</button>
       </div>
     </div>
-  `
+    `
+  }
+
+  if (level.story) {
+    return html`
+    <div class="${winPrefix}">
+      <div class="modalContent">
+        <h1>StoryBlabla Level ${level.level + 1}</h1> 
+        <p>Super awesome story here</p>
+        <button onclick=${() => send('level:closeStory')}>Start Tutorial</button>
+      </div>
+    </div>
+    `
   }
 
   return html`
     <div class="${goalPrefix}">
       <div class="modalContent">
         <h4>Level: ${level.level + 1}</h4>
-        <p>Collect 4 Gems</p>
+        <ul>${getGoals(level.goals)}</ul>
       </div>
     </div>
   `
+}
+
+function getGoals (goals) {
+  let goalString = ``
+
+  _.forEach(goals, (goal, key) => {
+    goalString += `<p>${'X'} ${key}: ${goal}</p>`
+  })
+
+  return goalString
 }
 
 module.exports = winningCondition

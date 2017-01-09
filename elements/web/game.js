7f7478295cdd9c49cfd3d1d529bf4798bc8e2d8b
Johannes Kahlhöfer <johannes.kahlhoefer@exxeta.com>
2017-01-09 20:49:08 +0100

    main page rendering in gameview for showing

--- elements/web/game.js
+++ elements/web/game.js
@@ -1,20 +1,23 @@
 const html = require('choo/html')
+const main = require('../../pages/main')
 
 function view (state, prev, send) {
-  return html`
-<div>
-    <div class="row">
-        <h1>Welcome ${state.client.username}!</h1>
-    </div>
-    <div class="row">
-        <textarea name="code" id="code">${state.client.code ? state.client.code : ''}</textarea>
-        <button onclick=${sendCode}>send as code</button>
-    </div>
-    <div class="row">
-        <button onclick=${cleanExit}>clean Exit</button>
-    </div>
-</div>
-`
+  return main(state, prev, send)
+
+//  return html`
+//<div>
+//    <div class="row">
+//        <h1>Welcome ${state.client.username}!</h1>
+//    </div>
+//    <div class="row">
+//        <textarea name="code" id="code">${state.client.code ? state.client.code : ''}</textarea>
+//        <button onclick=${sendCode}>send as code</button>
+//    </div>
+//    <div class="row">
+//        <button onclick=${cleanExit}>clean Exit</button>
+//    </div>
+//</div>
+//`
 
   function sendCode (event) {
     var code = document.getElementById('code').value

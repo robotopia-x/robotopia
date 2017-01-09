71aec65a6f4ee1117c68bbf4676717bc3fe36f4e
timgrossmann <t.grossmann1994@web.de>
2017-01-09 11:22:44 +0100

    renamed num to min-max / moved the random number to robot-api sensors

--- models/runtime/robot-api.js
+++ models/runtime/robot-api.js
@@ -141,6 +141,10 @@ module.exports = {
       const entity = getEntity(id, game)
 
       return getBaseOfTeam(game, entity.team.id).position
+    },
+
+    getRandomNumber: (state, id, min, max) => {
+      return Math.floor(Math.random() * (max - min + 1) + min)
     }
   }
 }

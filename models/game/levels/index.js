5759a96aafd2f05786dfcb2aba1d38b2d2cc6747
timgrossmann <t.grossmann1994@web.de>
2017-01-13 14:25:38 +0100

    added an easier level 3 to get into nested loops

--- models/game/levels/index.js
+++ models/game/levels/index.js
@@ -2,10 +2,12 @@ const editorState = require('./editor-state')
 const tutorial1State = require('./tutorial-1-state')
 const tutorial2State = require('./tutorial-2-state')
 const tutorial3State = require('./tutorial-3-state')
+const tutorial4State = require('./tutorial-4-state')
 
 module.exports = [
   editorState,
   tutorial1State,
   tutorial2State,
-  tutorial3State
+  tutorial3State,
+  tutorial4State
 ]

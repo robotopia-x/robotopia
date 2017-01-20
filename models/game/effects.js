c7f0a9a9002319bb9732b345da5484d1645f947f
timgrossmann <t.grossmann1994@web.de>
2017-01-20 14:37:26 +0100

    added reducer and effects for point increse/decrease

--- models/game/effects.js
+++ models/game/effects.js
@@ -8,9 +8,23 @@ function addResources (state, { teamId, amount }, send) {
 }
 
 function removeResources (state, { teamId, amount }, send) {
-  send('setResource', {
+  send('game:setResource', {
     teamId: teamId,
     amount: _.clamp(getTeamResources(state, teamId) - amount, 0, 10000)
+  }, _.noop)
+}
+
+function increaseGamePoints (state, { teamId, amount }, send) {
+  send('game:setGamePoints', {
+    teamId: teamId,
+    amount: getTeamGamePoints(state, teamId) + amount
+  }, _.noop)
+}
+
+function decreaseGamePoints (state, { teamId, amount }, send) {
+  send('game:setGamePoints', {
+    teamId: teamId,
+    amount: getTeamGamePoints(state, teamId) - amount
   }, _.noop)
 }
 
@@ -19,7 +33,13 @@ function getTeamResources ({ resources }, teamId) {
   return _.get(resources, teamId, 0)
 }
 
+function getTeamGamePoints ({ gamePoints }, teamId) {
+  return _.get(gamePoints, teamId, 0)
+}
+
 module.exports = {
   addResources,
-  removeResources
+  removeResources,
+  increaseGamePoints,
+  decreaseGamePoints
 }

fb6e452a344b136ec5c5af3fd7bc3f16522ef8a7
Johannes Kahlhöfer <johannes.kahlhoefer@exxeta.com>
2017-01-22 22:38:13 +0100

    X players are selected upon starting a game. currently X is 2 to form a 1v1

---
+++ elements/presenter-controls.js
@@ -0,0 +1,25 @@
+const html = require('choo/html')
+const button = require('./button')
+
+function startButtonView ({
+  isRunning,
+  onStart, onStop
+}) {
+  if (isRunning) {
+    return button({
+      onClick: onStop,
+      icon: 'stop',
+      label: 'Stop 1v1'
+    })
+  }
+
+  return button({
+    onClick: onStart,
+    icon: 'play',
+    label: 'Start 1v1'
+  })
+}
+
+module.exports = {
+  startButtonView
+}

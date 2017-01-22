1b52f6cb70ec07b0b3bf42e4e0c996484aeaeca2
Paul Sonnentag <paul.sonnentag@googlemail.com>
2017-01-22 19:33:25 +0100

    Fix double resource spots

--- models/game/effects.js
+++ models/game/effects.js
@@ -1,21 +1,24 @@
 const _ = require('lodash')
 const entites = require('./entities')
-const { getTeamResources, getTeamGamePoints, getGameState } = require('../../lib/game')
+const { getTeamResources, getTeamGamePoints, isFieldEmpty } = require('../../lib/game')
 
-function initializeResourceSpots ({ tiles }, { numberOfSpots }, send) {
+function initializeResourceSpots (state, { numberOfSpots }, send) {
+  const { tiles } = state
   const mapHeight = tiles.length
   const mapWidth = tiles[0].length
 
-  console.log(tiles)
+  const resourcePlaces = {}
 
   _.times(numberOfSpots, () => {
     let x, y
 
-    // search for random dirt spot
+    // search for empty random dirt spot
     do {
       x = _.random(0, mapWidth - 1)
       y = _.random(0, mapHeight - 1)
-    } while (tiles[y][x] !== 2)
+    } while (tiles[y][x] !== 2 || resourcePlaces[`${x},${y}`] === true)
+
+    resourcePlaces[`${x},${y}`] = true
 
     send('game:createEntity', { data: entites.gem({ x, y }) }, _.noop)
   })

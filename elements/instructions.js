6d6c90a12cf7eaac5a6a5597c1350d5db92bac88
timgrossmann <t.grossmann1994@web.de>
2017-02-16 15:57:42 +0100

    restructuring first tutorial / added tutorial Robot / changed arrows on move block

--- elements/instructions.js
+++ elements/instructions.js
@@ -5,13 +5,20 @@ const prefix = sf`
   :host {
     height: 100%;
     width: 100%;
+    padding: 25px;
+    margin: 0;
+  }
+  
+  :host > .instruction {
   }
 `
 
 function instructionView () {
   return html`
     <div class="${prefix}">
-      <h1>Hello World</h1>
+      <div class="instruction">
+        <h1>Hello World</h1>
+      </div>
     </div>
   `
 }

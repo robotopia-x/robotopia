257b598524c61d1e342af63818b9ce8c16f60014
Johannes Kahlhöfer <johannes.kahlhoefer@exxeta.com>
2017-02-19 16:36:51 +0100

    added file for level 5 tutorial levels will initialize upon being loaded -> enables passing on things from one level to another (via Saving onFinish and loading upon initialization)

--- assets/tutorial/01-blockly.js
+++ assets/tutorial/01-blockly.js
@@ -1,22 +1,23 @@
 const entities = require('../../models/game/entities')
 
-module.exports = {
-  game: {
-    tiles: [
-      [4],
-      [3],
-      [3],
-      [3],
-      [3]
-    ],
+module.exports = () => {
+  return {
+    game: {
+      tiles: [
+        [4],
+        [3],
+        [3],
+        [3],
+        [3]
+      ],
 
-    entities: [
-      entities.tutorialRobot({ x: 0, y: 4, id: 'ROBOT', orientation: 2 })
-    ]
-  },
+      entities: [
+        entities.tutorialRobot({ x: 0, y: 4, id: 'ROBOT', orientation: 2 })
+      ]
+    },
 
-  editor: {
-    workspace: `<xml xmlns="http://www.w3.org/1999/xhtml">
+    editor: {
+      workspace: `<xml xmlns="http://www.w3.org/1999/xhtml">
 <block type="start_handler" x="50" y="50" deletable="false">
     <statement name="body">
     <block type="move">
@@ -28,29 +29,30 @@ module.exports = {
 </block>
 </xml>`,
 
-    toolbox: `<xml id="toolbox" style="display: none">
+      toolbox: `<xml id="toolbox" style="display: none">
                 <category name="RemoveCategories" colour="40">
                     <block type="move"></block>
                 </category>
               </xml>`
-  },
+    },
 
-  label: 'Blockly',
+    label: 'Blockly',
 
-  goals: [
-    {
-      type: 'moveTo',
-      params: { position: { x: 0, y: 0 }, entity: 'ROBOT' },
-      desc: 'Move the robot to the stone tile',
-      isMandatory: true
-    }
-  ],
+    goals: [
+      {
+        type: 'moveTo',
+        params: {position: {x: 0, y: 0}, entity: 'ROBOT'},
+        desc: 'Move the robot to the stone tile',
+        isMandatory: true
+      }
+    ],
 
-  storyModal: {
-    text: 'Hey Morty, I build this robot to help us on our space adventures. I need your help programming it. Can you make it move to the grass field.',
-    hint: '',
-    img: 'assets/img/tutorials/simple-move.png'
-  },
+    storyModal: {
+      text: 'Hey Morty, I build this robot to help us on our space adventures. I need your help programming it. Can you make it move to the grass field.',
+      hint: '',
+      img: 'assets/img/tutorials/simple-move.png'
+    },
 
-  nextTutorial: 'turn'
+    nextTutorial: 'turn'
+  }
 }

06c6abbd474c3ce3aee28e6283a23f4ba7da30ae
Johannes Kahlhöfer <johannes.kahlhoefer@exxeta.com>
2017-02-19 17:23:24 +0100

    categories (wrong colors atm) added event block for ressource discovery to workspace

--- assets/tutorial/05-resource.js
+++ assets/tutorial/05-resource.js
@@ -1,7 +1,7 @@
 const entities = require('../../models/game/entities')
 
 const timeLimit = 10
-const DEFAULT_WORKSPACE = `<xml xmlns="http://www.w3.org/1999/xhtml"><block type="start_handler" x="50" y="50" deletable="false"></block></xml>`
+const DEFAULT_WORKSPACE = `<xml xmlns="http://www.w3.org/1999/xhtml"><block type="start_handler" x="50" y="50" deletable="false"></block><block type="resource_event_handler" x="400" y="400" deletable="false"></block></xml>`
 
 module.exports = () => {
   return {
@@ -45,17 +45,23 @@ module.exports = () => {
       workspace: getWorkspace(),
 
       toolbox: `<xml id="toolbox" style="display: none">
-                <category name="RemoveCategories" colour="40">
+                <category name="Movement" colour="40">
                     <block type="move"></block>
                     <block type="rotate"></block>
+                    <block type="move_to_entity"></block>
+                </category>
+                <category name="Actions" colour="50">
+                    <block type="collect_resource"></block>
+                    <block type="deposit_resource"></block>
+                </category>
+                <category name="Numbers" colour="80">
+                    <block type="math_number"></block>
+                    <block type="random_number"></block>
+                </category>
+                <category name="Logic" colour="30">
                     <block type="controls_repeat"></block>
                     <block type="controls_if"></block>
                     <block type="logic_compare"></block>
-                    <block type="math_number"></block>
-                    <block type="random_number"></block>
-                    <block type="move_to_entity"></block>
-                    <block type="collect_resource"></block>
-                    <block type="deposit_resource"></block>
                 </category>
               </xml>`
     },
@@ -87,11 +93,21 @@ module.exports = () => {
     try {
       wsJSON = JSON.parse(prevTut)
       if (wsJSON && wsJSON.workspace) {
-        return wsJSON.workspace
+        return addRessourceEventToWs(wsJSON.workspace)
       }
     } catch (e) {
       console.log(e)
     }
     return DEFAULT_WORKSPACE
   }
+
+  function addRessourceEventToWs (workspaceXML) {
+    let injectPart = '<block type="resource_event_handler" x="400" y="400" deletable="false">'
+    let injectBefore = '</xml>'
+    let newWS = workspaceXML.slice(0, workspaceXML.length - injectBefore.length)
+    newWS += injectPart
+    newWS += injectBefore
+    return newWS
+  }
+
 }

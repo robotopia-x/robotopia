0b25d094d95cbe490adcfe6b0ac7f58c4b35bbd0
Johannes Kahlhöfer <johannes.kahlhoefer@exxeta.com>
2017-02-19 20:15:31 +0100

    changed name of Category holding all Tiles to "Code Blocks" adjusted colors of categories for 05-resources changed workspace preset for 04

--- assets/tutorial/03-loop.js
+++ assets/tutorial/03-loop.js
@@ -35,7 +35,7 @@ module.exports = () => {
 </xml>`,
 
       toolbox: `<xml id="toolbox" style="display: none">
-                <category name="RemoveCategories" colour="40">
+                <category name="Code Blocks" colour="40">
                     <block type="move"></block>
                     <block type="rotate"></block>
                     <block type="controls_repeat"></block>

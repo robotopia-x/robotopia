abdb302172c2c4f33aa4c037d0d06649d69040c3
Johannes Kahlhöfer <johannes.kahlhoefer@exxeta.com>
2017-02-22 22:40:21 +0100

    styled win dialogue a little

--- elements/presenter-dialog.js
+++ elements/presenter-dialog.js
@@ -2,6 +2,7 @@ const _ = require('lodash')
 const html = require('choo/html')
 const modalView = require('./modal')
 const buttonView = require('./button')
+const sf = require('sheetify')
 
 module.exports = function ({
   presenter,
@@ -27,18 +28,43 @@ module.exports = function ({
   return html`<div></div>`
 
   function getPlayerPickingModal() {
+
+    const prefix = sf`
+      :host {
+        
+      }
+      
+      :host form {
+        display: inline-block;
+      }
+      
+      :host .pick_select {
+        height: 2em;
+      }
+      
+      :host .pick_option {
+        font-weight: 500;
+      }
+      
+      :host .cancel {
+        float: right
+      }
+`
+
+
     const startButton = buttonView({
       label: 'Start'
     })
 
     const cancelButton = buttonView({
       label: 'Cancel',
-      onClick: onCancelPlayerPicking
+      onClick: onCancelPlayerPicking,
+      additionalClasses: 'cancel'
     })
 
     return modalView(html`
-      <div>
-        <p>Chose the players to battle.</p>
+      <div class="${prefix}">
+        <h1 class="pick_header">Chose the players to battle.</h1>
         <form onsubmit=${handleSubmit}>
         ${clientsToOptions("1", presenter.clients, presenter.playerNumbers)}
         ${clientsToOptions("2", presenter.clients, presenter.playerNumbers)}
@@ -51,14 +77,15 @@ module.exports = function ({
 
     function clientsToOptions (selectSuffix, clients, players) {
       return html`
-        <select name="player${selectSuffix}">
+        <select name="player${selectSuffix}" class="pick_select">
+        <option selected disabled value="0">Player${selectSuffix}</option>
           ${Object.keys(clients).map(clientToOption)}
         </select>
       `
 
       function clientToOption (clientId) {
         //let isSelected = players[selectSuffix] === clientId
-        return html`<option value="${clientId}">${clients[clientId].username}</option>`
+        return html`<option class="pick_option" value="${clientId}">${clients[clientId].username}</option>`
       }
 
     }
@@ -69,6 +96,10 @@ module.exports = function ({
       const player2 = formData.get('player2')
 
       evt.preventDefault()
+
+      if (!player1 || !player2) {
+        return
+      }
 
       onPlayersPicked({
         1: player1,
@@ -108,6 +139,29 @@ module.exports = function ({
     if (!currentGame || !currentGame.teams) {
       return html`<div></div>`
     }
+
+    const prefix = sf`
+      :host .center {
+        color: #404040;
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
+      
+      :host > .logo {
+        width: 100%;
+        height: 150px;
+        background-image: url('assets/icons/robotopia.svg');
+        background-size: contain;
+        background-position: center;
+        background-repeat: no-repeat;
+        margin-bottom: 0;
+      }
+    `
+
     const teams = currentGame.teams
     const results = []
     let i
@@ -129,20 +183,17 @@ module.exports = function ({
       return a.resources < b.resources
     })
 
-    const winner = results[0]
-
-    console.log(results)
-
     const buttonHTML = buttonView({
       label: 'Close',
       onClick: onCloseWinModal
     })
 
     return modalView(html`
-      <div>
+      <div class="${prefix}">
+        <div class="logo"></div>
         <h1>Congratulations ${results[0].name}! You won!</h1>
-        <p> won the game scoring ${results[0].resources} points</p>
-        ${buttonHTML}
+        <p>${results[0].name} won the game scoring ${results[0].resources} points</p>
+        <div class="center">${buttonHTML}</div>
       </div>
     `)
 
